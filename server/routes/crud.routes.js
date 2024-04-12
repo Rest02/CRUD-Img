9b7258ca68e50e2e50a87c9daf141f1ff1b69157
Rest <code.rest.dev@gmail.com>
2024-04-12 16:57:09 -0400

    0.0.4 Guardar imagen en BD

--- server/routes/crud.routes.js
+++ server/routes/crud.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { upload } from '../multer.js'
+import { upload } from "../multer.js";
+import { pool } from "../db.js";
+import fs from "fs";
 
 const router = Router();
 
-router.post("/upload", upload.single("image") ,(req, res) => {
-  console.log(req.file)
+router.post("/upload", upload.single("image"), async (req, res) => {
+  const { filename } = req.file;
+  // const imagenBuffer = fs.readFileSync(`public/images/${filename}`);
+  const result = await pool.query(
+    "INSERT INTO imagetable(image) VALUES (?)",[filename]
+  );
+  console.log(result);
+  res.send("imagen subida con exito");
 });
 
 export default router;
